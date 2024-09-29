03411c6a5b3284cd482e0c5ffb1054cc8508611b
tomassirne <tomassirne@gmail.com>
2024-09-29 13:05:24 -0300

    Ejercicio 2 Listo

--- auxx.py
+++ auxx.py
@@ -2,5 +2,17 @@ a = "26SEP2019:09:24:50"
 
 print(a[5:9])
 
+t = ("N","F")
+if "M" in t:
+    if "F" in t: print("M-F")
+    elif "N" in t : print("M-N")
+    else: print("M-M")
+elif "F" in t :
+    if "N" in t : print("F-N")
+    else: print("F-F")
+else: print("N-N")
 
-print(min([5,2,9]))
+
+tu = (2,5,3)
+t[1] = 6
+print(t)
--- parcial.py
+++ parcial.py
@@ -41,6 +41,32 @@ def duracion_matrimonios_divorciados(divorcios):
 
 
 
+# ------ Ejercicio 2 ----------
+def promedio_por_tipos(divorcios):
+    #Creo un dic para cada tipo de matrimonio:  
+    # m -> masculino, f -> femenino, n-> no declara
+    dic_mm = {}  
+    dic_ff = {}  
+    dic_mf = {}  
+    dic_nn = {} 
+    dic_fn = {} 
+    dic_mn = {}  
+
+    for k,v in divorcios.items():
+        if "Masculino" in k:
+            if "Femenino" in k: dic_mf[k] = v 
+            elif "No declara" in k : dic_mn[k] = v 
+            else: dic_mm[k] = v
+        elif "Femenino" in k :
+            if "No declara" in k : dic_fn[k] = v
+            else: dic_ff[k] = v
+        else: dic_nn[k] = v
+
+    return {"Masculino-Masculino": duracion_promedio(dic_mm),"Masculino-Femenino": duracion_promedio(dic_mf),"Masculino-No declara": duracion_promedio(dic_mn),"Femenino-Femenino": duracion_promedio(dic_ff),"Femenino-No declara": duracion_promedio(dic_mm),"No declara-No declara": duracion_promedio(dic_mm)}
+
+
+
+
 # ------ Ejercicio 0 ----------
 dic = catalogar("dataset_divorcios.csv")
 
@@ -53,3 +79,19 @@ print(f"La duracion minima de matrimonios que se divorciaron es {duracion_matrim
 print(f"La duracion promedio de matrimonios que se divorciaron es {duracion_matrimonios_divorciados(dic)[2]:.2f} años.")
 
 
+
+# ------ Ejercicio 2 ----------
+
+"""
+Tipos de Matrimonio:
+    Masculino - Masculino
+    Masculino - Femenino
+    Femenino - Femenino
+    Masculino - No declara
+    Femenino - No declara
+    No declara - No declara
+"""
+
+prom_tipos = promedio_por_tipos(dic)
+for k,v in prom_tipos.items():
+    print(f"La duracion promedio de matrimonios {k} que se divorciaron es {v:.2f} años.")
