3a06a8becfff11d4c3201180ddc175254a3c369b
tomassirne <tomassirne@gmail.com>
2024-09-29 12:25:39 -0300

    Ejercicio 1 Listo

--- parcial.py
+++ parcial.py
@@ -1,6 +1,6 @@
 import csv
 
-
+# ------ Ejercicio 0 ----------
 def catalogar(archivo):
     with open(archivo, 'rt', encoding='utf-8-sig') as f:
         rows = csv.reader(f)
@@ -11,18 +11,45 @@ def catalogar(archivo):
             record = dict(zip(headers, row))
             if record["GENERO_1"] == "": record["GENERO_1"] = "No declara"
             if record["GENERO_2"] == "": record["GENERO_2"] = "No declara"
-        
-            clave = (record["GENERO_1"],record["GENERO_2"],record["FECHA_MATRIMONIO"][5:9],record["FECHA_CREACION"][5:9])
+            
+            #Hay dos registros cuya fecha de matrimonio es 01/01/1760 y fecha de divorcio es en 2024. 
+            #Asimismo hay un registro con fecha de matrimonio en el año 3201.
+            #Entendemos que son errores del dataset por lo tanto los filtraremos
+            if record["FECHA_MATRIMONIO"][5:9] == "1760" or record["FECHA_MATRIMONIO"][5:9] == "3201": pass
+            else: 
+                clave = (record["GENERO_1"],record["GENERO_2"],record["FECHA_MATRIMONIO"][5:9],record["FECHA_CREACION"][5:9])
             
 
-            if clave in diccionario: diccionario[clave] += 1
-            else: diccionario[clave] = 1
+                if clave in diccionario: diccionario[clave] += 1
+                else: diccionario[clave] = 1
         
-
     return diccionario
 
+
+# ------ Ejercicio 1 ----------
+def duracion_maxima(divorcios):
+    return max([int(ffin) - int(finicio) for (gen1,gen2,finicio,ffin) in divorcios.keys()])
+
+def duracion_minima(divorcios):
+    return min([int(ffin) - int(finicio) for (gen1,gen2,finicio,ffin) in divorcios.keys()])
+
+def duracion_promedio(divorcios):
+    return sum([(int(ffin) - int(finicio)) * divorcios[(gen1,gen2,finicio,ffin)] for (gen1,gen2,finicio,ffin) in divorcios.keys()]) / sum(divorcios.values())
+
+def duracion_matrimonios_divorciados(divorcios):
+    return duracion_maxima(divorcios), duracion_minima(divorcios), duracion_promedio(divorcios)
+
+
+
+# ------ Ejercicio 0 ----------
 dic = catalogar("dataset_divorcios.csv")
-print(dic)
-print(len(dic.keys()))
+
+
+
+# ------ Ejercicio 1 ----------
+duracion = duracion_matrimonios_divorciados(dic)
+print(f"La duracion maxima de matrimonios que se divorciaron es {duracion_matrimonios_divorciados(dic)[0]} años.")
+print(f"La duracion minima de matrimonios que se divorciaron es {duracion_matrimonios_divorciados(dic)[1]} años.")
+print(f"La duracion promedio de matrimonios que se divorciaron es {duracion_matrimonios_divorciados(dic)[2]:.2f} años.")
 
 
